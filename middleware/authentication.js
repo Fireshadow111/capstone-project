f8ba86f42d945d82b738928b86474886ad748b1b
Fireshadow111 <muhammadrajah16@gmail.com>
2024-03-22 16:55:30 +0200

    trying

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 const authenticate = (req,res,next) => {
     let {cookie} = req.headers
     let tokenInHeader = cookie && cookie.split('=')[1]
-    if(tokenInHeader===null) res.sendStatus(401)
+   
     console.log(tokenInHeader)
     jwt.verify(tokenInHeader, process.env.SECRET_KEY, (err, user)=>{
         if(err)return res.sendStatus(403)
